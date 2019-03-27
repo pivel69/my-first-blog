24c15c87b00e4950658fc82960b19ad8d591884e
pivel69 <pavel.panek@volny.cz>
2019-03-27 18:27:20 +0100

    Upravena sablona ukazuje clanky z databaze

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Clanky
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Clanky.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
